7e7c59803a826ef64626a19eb932d4f503160fbb
Nant1ng <andressantana99@hotmail.se>
2024-01-11 14:27:59 +0100

    Main Menu done

--- ProjectOne/App.cs
+++ ProjectOne/App.cs
@@ -4,7 +4,56 @@
     {
         public static void Run()
         {
-            
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine(@"
+                 ██████╗ █████╗ ██╗      ██████╗██╗   ██╗██╗      █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
+                ██╔════╝██╔══██╗██║     ██╔════╝██║   ██║██║     ██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
+                ██║     ███████║██║     ██║     ██║   ██║██║     ███████║   ██║   ██║██║   ██║██╔██╗ ██║
+                ██║     ██╔══██║██║     ██║     ██║   ██║██║     ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║
+                ╚██████╗██║  ██║███████╗╚██████╗╚██████╔╝███████╗██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║
+                 ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                                                                                   
+                                                    [1] Shapes
+
+                                                  [2] Calculator
+
+                                               [3] Rock Paper Scissors
+
+                                                     [0] Exit  
+                ");
+                Console.ResetColor();
+
+                char key = Console.ReadKey().KeyChar;
+                Console.Clear();
+
+                switch (key)
+                {
+                    case '1':
+
+                        break;
+
+                    case '2':
+
+                        break;
+
+                    case '3':
+
+                        break;
+
+                    case '0':
+                        Console.WriteLine("Exiting...");
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Invalid selection. Please choose a valid option.");
+                        Console.ResetColor();
+                        break;
+                }
+            } while (true);
         }
     }
 }
