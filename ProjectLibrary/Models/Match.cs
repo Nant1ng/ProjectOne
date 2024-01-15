dd6308555f869ed0435b55023899899cfc8c77b8
Nant1ng <andressantana99@hotmail.se>
2024-01-15 23:26:02 +0100

    Added Match Entity and Enums

---
+++ ProjectLibrary/Models/Match.cs
@@ -0,0 +1,21 @@
+﻿using ProjectLibrary.Enumeration;
+
+namespace ProjectLibrary.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public RockPaperScissor PlayerChoice { get; set; }
+        public RockPaperScissor ComputerChoice { get; set; }
+        public Result Result { get; set; }
+        public DateOnly GameDate { get; set; }
+
+        public Match(RockPaperScissor playerChoice, RockPaperScissor computerChoice, Result result, DateOnly gameDate)
+        {
+            PlayerChoice = playerChoice;
+            ComputerChoice = computerChoice;
+            Result = result;
+            GameDate = gameDate;
+        }
+    }
+}
